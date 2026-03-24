8b93670dfdd8aee208d2741c0083952e3208b870
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2026-03-24 11:28:41 -0600

    cambios realizados

---
+++ src/views/Login.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+const Login = () => {
+
+  return (
+    <Container className="mt-3">
+      <Row className="align-items-center">
+        <Col>
+          <h2><i className="bi-house-fill me-2"></i> Login</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Login;
