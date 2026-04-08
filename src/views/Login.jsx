c07fd00ac7853be2f227987a8be5337a6d30f503
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2026-04-07 21:04:04 -0600

    feat(login): implementar validación local y documentar integración con Supabase
    Se añade lógica de autenticación mediante un objeto de usuario local para facilitar el desarrollo. Se incluye el bloque de código de Supabase como comentario técnico para una rápida transición una vez que la base de datos esté operativa.

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,59 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import FormularioLogin from " .. /components/login/FormularioLogin";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Login = () => {
+  const [usuario, setUsuario] = useState("");
+  const [contrasena, setContrasena] = useState("");
+  const [error, setError] = useState(null);
+  const navegar = useNavigate();
+
+  const iniciarSesion = async () => {
+
+    const usuarioValido = { email: "admin@correo.com", password: "123" };
+
+    try {
+      if (usuario === usuarioValido.email && contrasena === usuarioValido.password) {
+        console.log("Sesión iniciada localmente");
+        localStorage.setItem("usuario-supabase", usuario);
+        navegar("/");
+      } else {
+        setError("Usuario o contraseña incorrectos (Modo Local)");
+      }
+
+      /*
+      2. Lógica de Supabase (Comentada hasta que la DB esté lista)
+        // recordar importar { supabase } al inicio del archivo he integrarlo dentro del proyecto con las credenciales .env
+      
+      try {
+        const { data, error } = await supabase.auth.signInWithPassword({
+          email: usuario,
+          password: contrasena,
+        });
+
+        if (error) {
+          setError("Usuario o contraseña incorrectos");
+          return;
+
+        }
+
+        if (data.user) {
+          localStorage.setItem("usuario-supabase", usuario);
+          navegar("/");
+
+        }
+      } catch (err) {
+        setError("Error al conectar con el servidor");
+        console.error("Error en la solicitud:", err);
+
+      };*/
+
+    } catch (err) {
+      setError("Error en el sistema de login");
+      console.error(err);
+    }
+  };
 
   return (
     <Container className="mt-3">
