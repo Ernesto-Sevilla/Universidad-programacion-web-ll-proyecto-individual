7ebcb307e83b821a71ee2723b089aa63198a558f
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2026-04-07 20:21:58 -0600

    feat(auth): crear estructura inicial del componente FormularioLogin
    Se añade el componente funcional FormularioLogin.jsx utilizando React y componentes de React-Bootstrap (Form, Button, Card, Alert). El componente está preparado para recibir props de estado y funciones de control de acceso.

---
+++ src/components/login/FormularioLogin.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+
+const FormularioLogin = ({ usuario, contrasena, error, setUsuario, setContrasena, iniciarSesion }) => {
+
+return ();
+
+};
+
+export default FormularioLogin;
